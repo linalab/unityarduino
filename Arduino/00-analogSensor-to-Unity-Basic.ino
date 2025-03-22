cc0b85f2e82c6a32e638e00bf2ad8e1d97f277d6
turbulente <citlali.hs@gmail.com>
2025-03-22 17:38:17 +0100

    Create 00-analogSensor-to-Unity-Basic.ino

---
+++ Arduino/00-analogSensor-to-Unity-Basic.ino
@@ -0,0 +1,21 @@
+/* Ejemplo básico para enviar un Float
+desde un sensor analógico a Unity
+utilizando la librería Cereal-io
+https://github.com/bernatGene/cereal-io*/
+// Arduino code
+#include "Cereal.h"
+Cereal cereal(9600);
+float valor;
+
+void setup() {
+  // put your setup code here, to run once:
+  cereal.begin();
+  pinMode(A0, INPUT);
+}
+
+void loop() {
+  valor = analogRead(A0);
+  valor=valor/1000;
+  cereal.sendFloat(0, valor);
+  cereal.passiveListen(100); 
+}
